21ea3f77f8b83aa14ce7661e5e355b9b90513505
AdamAmusa <92576185+AdamAmusa@users.noreply.github.com>
2024-10-25 09:54:20 +0000

    Updated login page and created services to handle authentication and tasks

--- w6-lab-firebase/src/app/login/login.page.ts
+++ w6-lab-firebase/src/app/login/login.page.ts
@@ -1,20 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
+
+import {
+  mailOutline,
+  lockClosedOutline,
+  eyeOutline,
+  eyeOffOutline,
+  logInOutline,
+  personAddOutline
+} from 'ionicons/icons';
+import { addIcons } from 'ionicons';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    IonicModule
+  ],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  private auth = inject(AuthService);
+  private router = inject(Router);
+  private fb = inject(FormBuilder);
 
-  constructor() { }
+  userAuthForm: FormGroup;
 
-  ngOnInit() {
+  constructor() {
+    addIcons({
+      mailOutline,
+      lockClosedOutline,
+      eyeOutline,
+      eyeOffOutline,
+      logInOutline,
+      personAddOutline
+    });
+
+    this.userAuthForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
   }
 
+  async handleRegistration() {
+    if (this.userAuthForm.valid) {
+      await this.auth.registerUser(this.userAuthForm.value);
+      await this.router.navigateByUrl('/home', { replaceUrl: true });
+    }
+  }
+
+  async handleAuthentication() {
+    if (this.userAuthForm.valid) {
+      await this.auth.authenticateUser(this.userAuthForm.value);
+      await this.router.navigateByUrl('/home', { replaceUrl: true });
+    }
+  }
+
+  async handlePasswordReset() {
+    const email = this.userAuthForm.get('email')?.value;
+    if (email && this.userAuthForm.get('email')?.valid) {
+      await this.auth.resetPassword(email);
+    }
+  }
 }
